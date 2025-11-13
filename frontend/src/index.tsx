7fdbcf4d297f43edfc21fe39993719fa137c82a5
Полуянов Андрей Сергеевич <rsandr77@gmail.com>
2025-11-13 14:21:46 +0500

    refactor(frontend): update index.tsx for Nginx/Docker compatibility

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
